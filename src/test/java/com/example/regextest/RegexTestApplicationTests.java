379b3c658a54fdb15072550d26a21e993869381b
jeongheeKim <8055kjh@gmail.com>
2023-02-22 23:57:01 +0900

    Initial commit

---
+++ src/test/java/com/example/regextest/RegexTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regextest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
